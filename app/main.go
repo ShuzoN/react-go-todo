adb6cb526ee740a0e9c78200465f342bad3ddaf5
ShuzoN <d.namu.r21@gmail.com>
2020-01-25 18:23:29 +0900

    sql driver

--- app/main.go
+++ app/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("/", fileServer)
+
+	db, err := sql.Open("mysql", "root:mysqlrootpassword@/headphonista")
 
 	log.Println("Listening...")
 	http.ListenAndServe(":80", nil)
