fdbd980a3caf403b9d5aa0473ebdd6a2d5cbf1a8
ShuzoN <d.namu.r21@gmail.com>
2020-01-26 22:24:49 +0900

    意味のない処理をコメントアウト

--- src/main.go
+++ src/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	// "database/sql"
+	// _ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 )
@@ -11,10 +11,9 @@ func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("/", fileServer)
 
-	db, _ := sql.Open("mysql", "root:mysqlrootpassword@/headphonista")
-	err := db.Ping()
-
-	log.Println(err)
+	// db, _ := sql.Open("mysql", "root:mysqlrootpassword@/headphonista")
+	// err := db.Ping()
+	// log.Println(err)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":80", nil)
