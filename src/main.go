ec730ea0f1ee39d014da6d5a2bae607aff414bd5
ShuzoN <d.namu.r21@gmail.com>
2020-01-30 23:34:32 +0900

    service & repository 次はコネクションクラスを作る

--- src/main.go
+++ src/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"log"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -15,12 +13,6 @@ func main() {
 
 	ds := databaseService()
 
-	ctx, stop := context.WithCancel(context.Background())
-	defer stop()
-
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": ds.getUserName(1),
@@ -30,34 +22,51 @@ func main() {
 
 }
 
-type DatabaseServeice struct {
-	db *sql.DB
+type DatabaseService struct {
+	databaseRepository *DatabaseRepository
 }
 
-func databaseService() *DatabaseServeice {
-	db, _ := sql.Open("mysql", "root:mysqlrootpassword@tcp(172.22.0.3:3306)/headphonista")
-
-	db.SetConnMaxLifetime(0)
-	db.SetMaxIdleConns(50)
-	db.SetMaxOpenConns(50)
-
-	err := db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ds := new(DatabaseServeice)
-	ds.db = db
-
+func databaseService() *DatabaseService {
+	ds := new(DatabaseService)
+	ds.databaseRepository = databaseRepository()
 	return ds
 }
 
-func (ds *DatabaseServeice) getUserName(id int) string {
+func (ds *DatabaseService) getUserName(id int) string {
 	var name string
-	err := ds.db.QueryRow("select p.name from users as p where p.id = ?;", id).Scan(&name)
+	err := ds.databaseRepository.getUserById(id).Scan(&name)
 	if err != nil {
 		log.Fatal("unable to execute search query", err)
 	}
 
 	return name
+}
+
+type DatabaseRepository struct {
+	db *sql.DB
+}
+
+func databaseRepository() *DatabaseRepository {
+	db, err := sql.Open("mysql", "root:mysqlrootpassword@tcp(172.22.0.3:3306)/headphonista")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.SetConnMaxLifetime(0)
+	db.SetMaxIdleConns(50)
+	db.SetMaxOpenConns(50)
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	databaseRepository := new(DatabaseRepository)
+	databaseRepository.db = db
+
+	return databaseRepository
+}
+
+func (databaseRepository *DatabaseRepository) getUserById(id int) *sql.Row {
+	return databaseRepository.db.QueryRow("select p.name from users as p where p.id = ?;", id)
 }
