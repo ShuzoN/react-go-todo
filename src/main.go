d4927112f3cc46ea1e302ca8cd81f8647d9c6b45
ShuzoN <d.namu.r21@gmail.com>
2020-02-01 18:00:03 +0900

    query params

--- src/main.go
+++ src/main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"headphonista/src/services"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type User struct {
+	ID int `uri:"id" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
 
 	ds := services.CreateUserService()
-
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": ds.GetUserName(1),
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello",
+		})
+	})
+	r.GET("/users/:id", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindUri(&user); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "page not found"})
+			return
+		}
+
+		name, err := ds.GetUserName(user.ID)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "page not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": name,
 		})
 	})
 	r.Run(":80")
